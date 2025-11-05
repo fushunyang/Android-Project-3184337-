91eafcf55d4f59553b1c5ff4c1ea84d50969105d
Shunyang Fu <924759663@qq.com>
2025-11-05 16:55:26 +0800

    Initial commit: Setup Android project with Jetpack Compose, Material3 and Navigation

--- app/src/main/java/com/example/tree/TreeManager.kt
+++
@@ -1,9 +0,0 @@
-    // Giant Tree Mode unlocked at 8000 steps
-    fun enterGiantTreeMode() {
-        if (currentSteps >= 8000) {
-            scaleFactor = 3.0f
-            enableRealTimeShadow = true
-            trunkWidthMultiplier = 3.0f
-            enableParticleEffects = true
-        }
-    }
